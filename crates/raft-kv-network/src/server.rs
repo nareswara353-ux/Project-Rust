4bf2ae73a8a93ec3fa381df4d0bf3d7b31176192
Narezzzs <nareswara353@gmail.com>
2026-09-14 20:01:05 +0700

    feat(network-server): implement async TCP server for RPC without comments

--- crates/raft-kv-network/src/server.rs
+++ crates/raft-kv-network/src/server.rs
@@ -1,63 +1,35 @@
 use crate::{NetworkError, RpcMessage};
-use raft_kv_core::message::{
-    AppendEntriesRequest, AppendEntriesResponse, InstallSnapshotRequest, InstallSnapshotResponse,
-    RequestVoteRequest, RequestVoteResponse,
-};
-use raft_kv_core::RaftError;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::RwLock;
-
-pub type VoteHandler = Arc<RwLock<dyn Fn(RequestVoteRequest) -> Result<RequestVoteResponse, RaftError> + Send + Sync>>;
-pub type AppendHandler = Arc<RwLock<dyn Fn(AppendEntriesRequest) -> Result<AppendEntriesResponse, RaftError> + Send + Sync>>;
-pub type SnapshotHandler = Arc<RwLock<dyn Fn(InstallSnapshotRequest) -> Result<InstallSnapshotResponse, RaftError> + Send + Sync>>;
+use tokio::sync::mpsc;
 
 pub struct RpcServer {
     addr: SocketAddr,
-    vote_handler: Option<VoteHandler>,
-    append_handler: Option<AppendHandler>,
-    snapshot_handler: Option<SnapshotHandler>,
+    handler: Arc<dyn Fn(RpcMessage) -> futures::future::BoxFuture<'static, Option<RpcMessage>> + Send + Sync>,
 }
 
 impl RpcServer {
-    pub fn new(addr: SocketAddr) -> Self {
+    pub fn new<F, Fut>(addr: SocketAddr, handler: F) -> Self
+    where
+        F: Fn(RpcMessage) -> Fut + Send + Sync + 'static,
+        Fut: futures::future::Future<Output = Option<RpcMessage>> + Send + 'static,
+    {
         Self {
             addr,
-            vote_handler: None,
-            append_handler: None,
-            snapshot_handler: None,
+            handler: Arc::new(move |msg| Box::pin(handler(msg))),
         }
-    }
-
-    pub fn with_vote_handler(mut self, handler: VoteHandler) -> Self {
-        self.vote_handler = Some(handler);
-        self
-    }
-
-    pub fn with_append_handler(mut self, handler: AppendHandler) -> Self {
-        self.append_handler = Some(handler);
-        self
-    }
-
-    pub fn with_snapshot_handler(mut self, handler: SnapshotHandler) -> Self {
-        self.snapshot_handler = Some(handler);
-        self
     }
 
     pub async fn run(self) -> Result<(), NetworkError> {
         let listener = TcpListener::bind(self.addr).await?;
-
         loop {
-            let (stream, peer_addr) = listener.accept().await?;
-            let vote_h = self.vote_handler.clone();
-            let append_h = self.append_handler.clone();
-            let snapshot_h = self.snapshot_handler.clone();
-
+            let (stream, _) = listener.accept().await?;
+            let handler = self.handler.clone();
             tokio::spawn(async move {
-                if let Err(e) = handle_connection(stream, vote_h, append_h, snapshot_h).await {
-                    eprintln!("Error handling connection from {}: {}", peer_addr, e);
+                if let Err(e) = handle_connection(stream, handler).await {
+                    eprintln!("Connection error: {}", e);
                 }
             });
         }
@@ -65,56 +37,25 @@ impl RpcServer {
 }
 
 async fn handle_connection(
-    mut stream: TcpStream,
-    vote_handler: Option<VoteHandler>,
-    append_handler: Option<AppendHandler>,
-    snapshot_handler: Option<SnapshotHandler>,
+    stream: TcpStream,
+    handler: Arc<dyn Fn(RpcMessage) -> futures::future::BoxFuture<'static, Option<RpcMessage>> + Send + Sync>,
 ) -> Result<(), NetworkError> {
-    let mut buf = [0u8; 4096];
-    let n = stream.read(&mut buf).await?;
-    if n == 0 {
-        return Ok(());
+    let (mut reader, mut writer) = stream.into_split();
+
+    let mut len_buf = [0u8; 4];
+    while reader.read_exact(&mut len_buf).await.is_ok() {
+        let msg_len = u32::from_be_bytes(len_buf) as usize;
+        let mut buf = vec![0u8; msg_len];
+        reader.read_exact(&mut buf).await?;
+
+        let msg = crate::codec::decode_message(&buf)?;
+        if let Some(response) = handler(msg).await {
+            let resp_bytes = crate::codec::encode_message(&response)?;
+            let resp_len = resp_bytes.len() as u32;
+            writer.write_all(&resp_len.to_be_bytes()).await?;
+            writer.write_all(&resp_bytes).await?;
+        }
     }
-
-    let msg: RpcMessage = bincode::deserialize(&buf[..n])
-        .map_err(|e| NetworkError::Serialization(e.to_string()))?;
-
-    let response = match msg {
-        RpcMessage::RequestVote(req) => {
-            if let Some(handler) = vote_handler {
-                let h = handler.read().await;
-                let res = h(req).map_err(|e| NetworkError::Connection(e.to_string()))?;
-                RpcMessage::RequestVoteResponse(res)
-            } else {
-                return Err(NetworkError::Connection("No vote handler registered".into()));
-            }
-        }
-        RpcMessage::AppendEntries(req) => {
-            if let Some(handler) = append_handler {
-                let h = handler.read().await;
-                let res = h(req).map_err(|e| NetworkError::Connection(e.to_string()))?;
-                RpcMessage::AppendEntriesResponse(res)
-            } else {
-                return Err(NetworkError::Connection("No append handler registered".into()));
-            }
-        }
-        RpcMessage::InstallSnapshot(req) => {
-            if let Some(handler) = snapshot_handler {
-                let h = handler.read().await;
-                let res = h(req).map_err(|e| NetworkError::Connection(e.to_string()))?;
-                RpcMessage::InstallSnapshotResponse(res)
-            } else {
-                return Err(NetworkError::Connection("No snapshot handler registered".into()));
-            }
-        }
-        _ => return Err(NetworkError::Connection("Unknown message type".into())),
-    };
-
-    let resp_bytes = bincode::serialize(&response)
-        .map_err(|e| NetworkError::Serialization(e.to_string()))?;
-    
-    stream.write_all(&resp_bytes).await?;
-    stream.flush().await?;
 
     Ok(())
 }
